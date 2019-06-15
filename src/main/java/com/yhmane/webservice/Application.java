ea488ba5bba31092842a2ad4800d4de4d0d651e0
yhmane <hwang9yh@gmail.com>
2019-06-16 03:11:55 +0900

    server config location change

--- src/main/java/com/yhmane/webservice/Application.java
+++ src/main/java/com/yhmane/webservice/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 			+ "classpath:application.yml,"
-			+ "/app/config/boot-webservice/real-application.yml";
+			+ "/app/config/spring-webservice/real-application.yml";
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class)
